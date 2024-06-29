67316192883c8f6bf748ce04eeae39689dd31a40
justmedusty <dustyn_gibb@hotmail.com>
2024-06-29 05:28:26 -0400

    vmmm

--- kernel/src/arch/x86_64/mem/arch_vmm.c
+++ kernel/src/arch/x86_64/mem/arch_vmm.c
@@ -23,6 +23,10 @@ static pte_t* walkpgdir(p4d_t *pgdir, const uint64 *va,int alloc){
     pud = &pgdir[PUDX(va)] + hhdm_offset;
 
     if(!pud || !(*pud & PTE_P)){
+
+        if(alloc){
+            pud = phys_alloc(1) + hhdm_offset;
+        }
         return 0;
     }
 
@@ -66,22 +70,40 @@ void *get_phys_addr(uint64 *va) {
 /*
  * Boiler plate right now will change obviously
  */
-void map_pages(p4d_t *page_dir, uint64 *physaddr, uint64 *va, unsigned int flags,uint32 size) {
+void map_pages(p4d_t *pgdir, uint64 *physaddr, uint64 *va, unsigned int flags,uint32 size) {
     // Make sure that both addresses are page-aligned.
+
+    if(!pgdir){
+        return 0;
+    }
+
+    pte_t result;
 
     pud_t *pud;
     pmd_t *pmd;
     pte_t *pte;
 
-    pud = &page_dir[PUDX(va)];
-    pmd = &pud[PMDX(va)];
-    pte = &pmd[PTX(va)];
+    pud = &pgdir[PUDX(va)] + hhdm_offset;
+
+    if(!pud || !(*pud & PTE_P)){
+
+        return 0;
+    }
+
+    pmd = PTE_ADDR(pud[PMDX(va)] + hhdm_offset);
+
+    if(!pmd || !(*pmd & PTE_P)){
+        return 0;
+    }
+
+    pte = PTE_ADDR(pmd[PTX(va)] + hhdm_offset);
 
 
 
 
 
-   // pt[ptindex] = ((unsigned long)physaddr) | (flags & 0xFFF) | 0x01; // Present
+
+    // pt[ptindex] = ((unsigned long)physaddr) | (flags & 0xFFF) | 0x01; // Present
 
     // Now you need to flush the entry in the TLB
     // or you might not notice the change.
