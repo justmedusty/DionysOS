4c9b23418dd4b3b1d3273d56164df527b7810811
dustyn <dustyn_gibb@hotmail.com>
2024-07-12 09:03:10 +0000

    going to start adding region support to the vm manager

--- kernel/src/include/arch_vmm.h
+++ kernel/src/include/arch_vmm.h
@@ -2,14 +2,18 @@
 // Created by dustyn on 6/25/24.
 //
 
-#ifndef KERNEL_ARCH_VMM_H
-#define KERNEL_ARCH_VMM_H
 #pragma once
 
-static inline void __native_flush_tlb_single(unsigned long addr) {
-    asm volatile("invlpg (%0)" ::"r" (addr) : "memory");
+static inline void native_flush_tlb_single(unsigned long vaddr) {
+    asm volatile("invlpg (%0)" ::"r" (vaddr) : "memory");
+}
+
+static inline void native_flush_tlb_range(unsigned long vaddr,uint64 pages) {
+    for(int i = 0; i < pages; i++){
+        asm volatile("invlpg (%0)" ::"r" (vaddr + (i * PAGE_SIZE)) : "memory");
+    }
+
 }
 void arch_init_vmm();
 void switch_page_table(p4d_t *page_dir);
 int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint32 perms,uint64 size);
-#endif //KERNEL_ARCH_VMM_H
