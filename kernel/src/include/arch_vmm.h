53781684915402b2b2dd093d5745526208c4c5db
dustyn <dustyn_gibb@hotmail.com>
2024-07-12 09:26:06 +0000

    region definition and sections

--- kernel/src/include/arch_vmm.h
+++ kernel/src/include/arch_vmm.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+
 static inline void native_flush_tlb_single(unsigned long vaddr) {
     asm volatile("invlpg (%0)" ::"r" (vaddr) : "memory");
 }
@@ -12,8 +13,29 @@ static inline void native_flush_tlb_range(unsigned long vaddr,uint64 pages) {
     for(int i = 0; i < pages; i++){
         asm volatile("invlpg (%0)" ::"r" (vaddr + (i * PAGE_SIZE)) : "memory");
     }
-
 }
+
+typedef struct virtual_region {
+    uint64 va;
+    uint64 pa;
+    uint64 end_addr;
+    uint64 num_pages;
+    uint64 flags;
+
+    uint64 ref_count;
+
+    struct vm_region* next;
+    struct vm_region* prev;
+
+} virtual_region;
+
+extern text_start;
+extern text_end;
+extern rodata_start;
+extern rodata_end;
+extern data_start;
+extern data_end;
+
 void arch_init_vmm();
 void switch_page_table(p4d_t *page_dir);
 int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint32 perms,uint64 size);
