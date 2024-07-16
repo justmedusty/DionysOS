7fa74eea0cbc965ac51ed8f939872b141cda29dc
dustyn <dustyn_gibb@hotmail.com>
2024-07-16 09:02:40 +0000

    fixing mapping

--- kernel/src/arch/x86_64/mem/arch_vmm.c
+++ kernel/src/arch/x86_64/mem/arch_vmm.c
@@ -21,18 +21,18 @@
 p4d_t *global_pg_dir = 0;
 struct virt_map *kernel_pg_map;
 
-void switch_page_table(p4d_t *page_dir){
+void switch_page_table(p4d_t *page_dir) {
     lcr3(page_dir);
 }
 
-void arch_init_vmm(){
+void arch_init_vmm() {
 
-    kernel_pg_map = (struct virt_map*)P2V(phys_alloc(1));
-    memset(kernel_pg_map,0,PAGE_SIZE);
+    kernel_pg_map = (struct virt_map *) P2V(phys_alloc(1));
+    memset(kernel_pg_map, 0, PAGE_SIZE);
     kernel_pg_map->top_level = P2V(phys_alloc(1));
-    memset(kernel_pg_map->top_level,0,PAGE_SIZE);
+    memset(kernel_pg_map->top_level, 0, PAGE_SIZE);
     kernel_pg_map->vm_region_head = P2V(phys_alloc(1));
-    memset(kernel_pg_map->vm_region_head,0,PAGE_SIZE);
+    memset(kernel_pg_map->vm_region_head, 0, PAGE_SIZE);
 
     kernel_pg_map->vm_region_head->next = kernel_pg_map->vm_region_head;
     kernel_pg_map->vm_region_head->prev = kernel_pg_map->vm_region_head;
@@ -40,36 +40,44 @@ void arch_init_vmm(){
     /*
      * Map symbols in the linker script
      */
-    uint64 k_text_start = ALIGN_DOWN((uint64*)text_start,PAGE_SIZE);
-    uint64 k_text_end = ALIGN_UP((uint64*)text_end,PAGE_SIZE);
+    uint64 k_text_start = ALIGN_DOWN((uint64 *) text_start, PAGE_SIZE);
+    uint64 k_text_end = ALIGN_UP((uint64 *) text_end, PAGE_SIZE);
 
-    uint64 k_rodata_start =  ALIGN_DOWN((uint64*)rodata_start,PAGE_SIZE);
-    uint64 k_rodata_end = ALIGN_UP((uint64*)rodata_end,PAGE_SIZE);
+    uint64 k_rodata_start = ALIGN_DOWN((uint64 *) rodata_start, PAGE_SIZE);
+    uint64 k_rodata_end = ALIGN_UP((uint64 *) rodata_end, PAGE_SIZE);
 
-    uint64 k_data_start = ALIGN_DOWN((uint64*)data_start,PAGE_SIZE);
-    uint64 k_data_end = ALIGN_UP((uint64*)data_end,PAGE_SIZE);
+    uint64 k_data_start = ALIGN_DOWN((uint64 *) data_start, PAGE_SIZE);
+    uint64 k_data_end = ALIGN_UP((uint64 *) data_end, PAGE_SIZE);
 
-    for(uint64 text = k_text_start; text < k_text_end; text += PAGE_SIZE){
-        map_pages(kernel_pg_map->top_level,text,text - kernel_min + kernel_phys_min,PTE_P,text_end - text_start);
+
+    if (map_pages(kernel_pg_map->top_level, k_text_start, k_text_start - kernel_min + kernel_phys_min, PTE_P, k_text_end - k_text_start) == -1) {
+        panic("Mapping text!");
+    }
+    if (map_pages(kernel_pg_map->top_level, k_text_start, k_text_start - kernel_min + kernel_phys_min, PTE_P | PTE_NX, k_rodata_end - k_text_start) == -1) {
+        panic("Mapping rodata!");
     }
 
-    for(uint64 rodata = k_rodata_start; rodata < k_rodata_end; rodata += PAGE_SIZE){
-        map_pages(kernel_pg_map->top_level,rodata,rodata - kernel_min + kernel_phys_min,PTE_P | PTE_NX, rodata_end - rodata_start);
+    if (map_pages(kernel_pg_map->top_level, k_data_start, k_data_start - kernel_min + kernel_phys_min, PTE_P | PTE_NX | PTE_RW, k_data_end - k_data_start) == -1) {
+        panic("Mapping data!");
     }
-
+    /*
+     * Map the first 4gb to the higher va space for the kernel, for the hhdm mapping
+     */
+    if(map_pages(kernel_pg_map->top_level, 0, 0, PTE_P | PTE_RW, 0xFFFFFFFF) == -1){
+        panic("Mapping first 4gb!");
+    }
 
 
 }
-
 
 
 /*
  * Walk a page directory to find a physical address, or allocate all the way down if the alloc flag is set
  */
 
-static pte_t* walkpgdir(p4d_t *pgdir, const uint64 *va,int alloc){
+static pte_t *walkpgdir(p4d_t *pgdir, const uint64 *va, int alloc) {
 
-    if(!pgdir){
+    if (!pgdir) {
         return 0;
     }
 
@@ -81,30 +89,30 @@ static pte_t* walkpgdir(p4d_t *pgdir, const uint64 *va,int alloc){
 
     pud = V2P(PTE_ADDR(pgdir[PUDX(va)]));
 
-    if(!pud || !(*pud & PTE_P)){
-        if(alloc){
-             *pud = (uint64) kalloc(8) | PTE_P | PTE_RW | PTE_U;
-        } else{
+    if (!pud || !(*pud & PTE_P)) {
+        if (alloc) {
+            *pud = (uint64) kalloc(8) | PTE_P | PTE_RW | PTE_U;
+        } else {
             return 0;
         }
     }
 
     pmd = V2P(PTE_ADDR(pud[PMDX(va)]));
 
-    if(!pmd || !(*pmd & PTE_P)){
-        if(alloc){
+    if (!pmd || !(*pmd & PTE_P)) {
+        if (alloc) {
             *pmd = (uint64) kalloc(8) | PTE_P | PTE_RW | PTE_U;
-        } else{
+        } else {
             return 0;
         }
     }
 
     pte = V2P(PTE_ADDR(pmd[PTX(va)]));
 
-    if(!pte || !(*pte & PTE_P)){
-        if(alloc){
+    if (!pte || !(*pte & PTE_P)) {
+        if (alloc) {
             *pte = (uint64) kalloc(8) | PTE_P | PTE_RW | PTE_U;
-        } else{
+        } else {
             return 0;
         }
     }
@@ -114,26 +122,26 @@ static pte_t* walkpgdir(p4d_t *pgdir, const uint64 *va,int alloc){
 /*
  * Maps pages from VA/PA to size in page size increments.
  */
-int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms,uint64 size) {
+int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 size) {
 
-    uint64 *address, *last;
+    uint64 * address, *last;
     pte_t *pte;
-    address = PGROUNDDOWN((uint64)va);
-    last = PGROUNDDOWN(((uint64)va) + size - 1);
+    address = PGROUNDDOWN((uint64) va);
+    last = PGROUNDDOWN(((uint64) va) + size - 1);
 
-    for(;;){
+    for (;;) {
 
-        if((pte = walkpgdir(pgdir, address, 1)) == 0){
+        if ((pte = walkpgdir(pgdir, address, 1)) == 0) {
             return -1;
         }
 
-        if(*pte & PTE_P) {
+        if (*pte & PTE_P) {
             panic("remap");
         }
 
         *pte = physaddr | perms | PTE_P;
 
-        if(address == last){
+        if (address == last) {
             break;
         }
 
@@ -143,6 +151,6 @@ int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms,uint64 siz
     return 0;
 }
 
-struct vm_region *create_region(){
+struct vm_region *create_region() {
 
 }
