648cd70f6625fabfa6b338c972d272883b0f9fd3
dustyn <dustyn_gibb@hotmail.com>
2024-07-18 09:21:08 +0000

    putting panic back, there are overlapping address ranges so need to look into that in the linker script addresses and see why they are overlapping

--- kernel/src/arch/x86_64/mem/arch_vmm.c
+++ kernel/src/arch/x86_64/mem/arch_vmm.c
@@ -43,17 +43,17 @@ void arch_init_vmm() {
     uint64 k_text_start = ALIGN_DOWN((uint64 *) &text_start, PAGE_SIZE);
     uint64 k_text_end = ALIGN_UP((uint64 *) &text_end, PAGE_SIZE);
 
-    serial_printf("Ktext start %x.64 , Ktext end %x.64\n",k_text_start,k_text_end);
+    serial_printf("  Ktext start %x.64 , Ktext end %x.64\n",k_text_start,k_text_end);
 
     uint64 k_rodata_start = ALIGN_DOWN((uint64 *) rodata_start, PAGE_SIZE);
     uint64 k_rodata_end = ALIGN_UP((uint64 *) rodata_end, PAGE_SIZE);
 
-    serial_printf("Krodata start %x.64 , Krodata end %x.64\n",k_rodata_start,k_rodata_end);
+    serial_printf("Krodata start %x.64,Krodata end %x.64\n",k_rodata_start,k_rodata_end);
 
     uint64 k_data_start = ALIGN_DOWN((uint64 *) data_start, PAGE_SIZE);
     uint64 k_data_end = ALIGN_UP((uint64 *) data_end, PAGE_SIZE);
 
-    serial_printf("Kdata start %x.64 , Kdata end %x.64\n",k_data_start,k_data_end);
+    serial_printf("  Kdata start %x.64 , Kdata end %x.64\n",k_data_start,k_data_end);
 
     serial_printf("Mapping kernel text\n");
     if (map_pages(kernel_pg_map->top_level, k_text_start - kernel_min + kernel_phys_min,k_text_start, 0, k_text_end - k_text_start) == -1) {
@@ -162,12 +162,11 @@ int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 si
 
     for (;;) {
         if ((pte = walkpgdir(pgdir, address, 1)) == 0) {
-
             return -1;
-
         }
-
-
+        if(*pte & PTE_P){
+            panic("remap");
+        }
 
         *pte = physaddr | perms | PTE_P;
 
