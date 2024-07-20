53448445411ce1abc788b26b0c1041efd66b447b
dustyn <dustyn_gibb@hotmail.com>
2024-07-20 22:49:58 +0000

    got the text mapping working

--- kernel/src/arch/x86_64/mem/arch_vmm.c
+++ kernel/src/arch/x86_64/mem/arch_vmm.c
@@ -54,21 +54,21 @@ void arch_init_vmm() {
     uint64 k_data_end = PGROUNDUP(data_end);
 
     serial_printf("  Kdata start %x.64 , Kdata end %x.64\n",k_data_start,k_data_end);
-
     serial_printf("Mapping kernel text Start: %x.64 End: %x.64\n",k_text_start,k_text_end);
 
-    if (map_pages(kernel_pg_map->top_level, k_text_start - kernel_min + kernel_phys_min, k_text_start, 0, k_text_end - k_text_start) == -1) {
+    if (map_pages(kernel_pg_map->top_level, text_start - kernel_min + kernel_phys_min, k_text_start, 0, k_text_end - k_text_start) == -1) {
         panic("Mapping text!");
     }
-    panic("done");
-    serial_printf("Mapping kernel rodata Start: %x.64 End: %x.64\n",k_rodata_start,(k_rodata_start - kernel_min + kernel_phys_min) + (k_rodata_end - k_rodata_start));
 
-    if (map_pages(kernel_pg_map->top_level, V2P(k_rodata_start),P2V(k_rodata_start), PTE_NX, k_rodata_end - k_text_start) == -1) {
+    serial_printf("Mapping kernel rodata Start: %x.64 End: %x.64\n",k_rodata_start,k_rodata_end);
+    panic("done");
+    if (map_pages(kernel_pg_map->top_level, k_rodata_start - kernel_min + kernel_phys_min,k_rodata_start, PTE_NX, k_rodata_end - k_text_start) == -1) {
         panic("Mapping rodata!");
     }
 
-    serial_printf("Mapping kernel data Start: %x.64 End: %x.64\n",k_data_start - kernel_min + kernel_phys_min,(k_data_start - kernel_min + kernel_phys_min) + (k_data_end - k_data_start) );
-    if (map_pages(kernel_pg_map->top_level, k_data_start - kernel_min + kernel_phys_min,k_data_start, PTE_NX | PTE_RW, k_data_end - k_data_start) == -1) {
+    serial_printf("Mapping kernel data Start: %x.64 End: %x.64\n",(k_data_start - kernel_min + kernel_phys_min) + (k_data_end - k_data_start) );
+
+    if (map_pages(kernel_pg_map->top_level, V2P(k_data_start),k_data_start, PTE_NX | PTE_RW, k_data_end - k_data_start) == -1) {
         panic("Mapping data!");
     }
 
@@ -78,8 +78,8 @@ void arch_init_vmm() {
     if(map_pages(kernel_pg_map->top_level, 0, 0, PTE_P | PTE_RW, 0xFFFFFFFF) == -1){
         panic("Mapping first 4gb!");
     }
-
     serial_printf("Kernel page table built\n");
+
 
 
 }
@@ -89,8 +89,7 @@ void arch_init_vmm() {
  * Walk a page directory to find a physical address, or allocate all the way down if the alloc flag is set
  */
 
-static pte_t *walkpgdir(p4d_t *pgdir, const uint64 *va, int alloc) {
-    serial_printf("va is %x.64\n",va);
+static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
     if (!pgdir) {
         return 0;
     }
@@ -154,11 +153,12 @@ static pte_t *walkpgdir(p4d_t *pgdir, const uint64 *va, int alloc) {
 int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 size) {
 
 
-    uint64 *address, *last;
+    char *address, *last;
     pte_t *pte;
     address = PGROUNDDOWN((uint64) va);
     last = PGROUNDDOWN(((uint64) va) + size - 1);
     serial_printf("address : %x.64 last : %x.64 size : %x.64\n",address,last,size);
+
 
     for (;;) {
         if ((pte = walkpgdir(pgdir, address, 1)) == 0) {
@@ -166,13 +166,19 @@ int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 si
         }
 
         if(*pte & PTE_P){
+            *pte &= ~PTE_P;
             serial_printf("Remap! Address being remapped is %x.64\n",*pte);
-            panic("remap");
+            //panic("remap");
         }
 
         *pte = physaddr | perms | PTE_P;
 
+        if(address > last){
+            serial_printf("address : %x.64 last : %x.64 \n",address,last);
+            panic("here");
+        }
         if (address == last) {
+
             break;
         }
 
@@ -181,7 +187,7 @@ int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 si
 
 
     }
-    panic("done");
+
     return 0;
 }
 
