6bf93b48d0704e598fd0d9ea2e787cf4442e57b8
dustyn <dustyn_gibb@hotmail.com>
2024-07-26 22:08:59 +0000

    Need to figure out why the hhdm mapping doesn't work now

--- kernel/src/arch/x86_64/mem/arch_vmm.c
+++ kernel/src/arch/x86_64/mem/arch_vmm.c
@@ -39,46 +39,47 @@ void arch_init_vmm() {
     /*
      * Map symbols in the linker script
      */
-    uint64 k_text_start = PGROUNDDOWN(&text_start);
-    uint64 k_text_end = PGROUNDUP(&text_end);
+    uint64 k_text_start = ALIGN_DOWN((uint64) &text_start, PAGE_SIZE);
+    uint64 k_text_end = ALIGN_UP((uint64) &text_end,PAGE_SIZE);
 
     serial_printf("  Ktext start %x.64 , Ktext end %x.64\n", k_text_start, k_text_end);
 
-    uint64 k_rodata_start = PGROUNDDOWN(&rodata_start);
-    uint64 k_rodata_end = PGROUNDUP(&rodata_end);
+    uint64 k_rodata_start =  ALIGN_DOWN((uint64) &rodata_start, PAGE_SIZE);
+    uint64 k_rodata_end =  ALIGN_UP((uint64) &rodata_end, PAGE_SIZE);
 
     serial_printf("Krodata start %x.64,Krodata end %x.64\n", k_rodata_start, k_rodata_end);
 
-    uint64 k_data_start = PGROUNDDOWN(&data_start);
-    uint64 k_data_end = PGROUNDUP(&data_end);
+    uint64 k_data_start =  ALIGN_DOWN((uint64) &data_start, PAGE_SIZE);
+    uint64 k_data_end =  ALIGN_UP((uint64) &data_end, PAGE_SIZE);
     serial_printf("  Kdata start %x.64 , Kdata end %x.64\n", k_data_start, k_data_end);
 
-    serial_printf("Mapping kernel text Start: %x.64 End: %x.64\n", text_start, &text_end);
-    if (map_pages(kernel_pg_map->top_level, (text_start - kernel_min) + kernel_phys_min, (uint64 *) text_start, 0,(uint64) text_end - (uint64) text_start) == -1) {
+
+    if (map_pages(kernel_pg_map->top_level, (k_text_start - kernel_min) + kernel_phys_min, (uint64 *) k_text_start, 0,(uint64) k_text_end - (uint64) k_text_start,1) == -1) {
         panic("Mapping text!");
     }
+    serial_printf("Kernel Text Mapped\n");
 
-    serial_printf("Mapping kernel rodata Start: %x.64 End: %x.64\n", rodata_start, rodata_end);
 
-    if (map_pages(kernel_pg_map->top_level, (k_rodata_start - kernel_min) + kernel_phys_min, (uint64 *) rodata_start,PTE_NX, (uint64) rodata_end - (uint64) text_start) == -1) {
+    if (map_pages(kernel_pg_map->top_level, (k_rodata_start - kernel_min) + kernel_phys_min, (uint64 *) k_rodata_start,PTE_NX, (uint64) k_rodata_end - (uint64) k_rodata_start,1) == -1) {
         panic("Mapping rodata!");
     }
+    serial_printf("Kernel Rodata Mapped\n");
 
-    serial_printf("Mapping kernel data Start: %x.64 End: %x.64\n", data_start, data_end);
 
-    if (map_pages(kernel_pg_map->top_level, (k_data_start - kernel_min) + kernel_phys_min, (uint64 *) data_start,
-                  PTE_NX | PTE_RW, (uint64) data_end - (uint64) data_start) == -1) {
+    if (map_pages(kernel_pg_map->top_level, (k_data_start - kernel_min) + kernel_phys_min, (uint64 *) k_data_start,PTE_NX | PTE_RW, (uint64) k_data_end - (uint64) k_data_start,1) == -1) {
         panic("Mapping data!");
     }
+    serial_printf("Kernel Data Mapped\n");
 
     /*
      * Map the first 4gb to the higher va space for the kernel, for the hhdm mapping
      */
     serial_printf("Mapping HHDM\n");
 
-    if (map_pages(kernel_pg_map->top_level, 0, 0 + hhdm_offset, PTE_RW | PTE_NX, 0x100000000) == -1) {
+    if (map_pages(kernel_pg_map->top_level, 0, 0 + hhdm_offset, PTE_RW | PTE_NX, 0x100000000,1) == -1) {
         panic("Mapping first 4gb!");
     }
+    serial_printf("Kernel HHDM Mapped\n");
 
     struct limine_memmap_response *memmap = memmap_request.response;
     struct limine_memmap_entry **entries = memmap->entries;
@@ -93,7 +94,7 @@ void arch_init_vmm() {
             if (j < (uint64) 0x100000000) {
                 continue;
             }
-            if (map_pages(kernel_pg_map->top_level, j, j + hhdm_offset, PTE_NX | PTE_RW, PAGE_SIZE) == -1) {
+            if (map_pages(kernel_pg_map->top_level, j, j + hhdm_offset, PTE_NX | PTE_RW, PAGE_SIZE,1) == -1) {
                 panic("hhdm mapping");
             }
 
@@ -114,7 +115,7 @@ void arch_init_vmm() {
  * Walk a page directory to find a physical address, or allocate all the way down if the alloc flag is set
  */
 
-static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
+static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc,int init) {
     if (!pgdir) {
         return 0;
     }
@@ -127,8 +128,7 @@ static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
 
     p4d_t *p4d = &pgdir[p4d_idx];
 
-
-    if(!(*p4d & PTE_P)){
+    if(!(*p4d & PTE_P) || init){
         if (alloc) {
             *p4d = (uint64) kalloc(PAGE_SIZE);
             *p4d |= PTE_P | PTE_RW | PTE_U;
@@ -138,7 +138,7 @@ static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
     }
     pud_t *pud = &p4d[pud_idx];
 
-    if (!(*pud & PTE_P)) {
+    if (!(*pud & PTE_P) || init) {
         if (alloc) {
             *pud = (uint64) kalloc(PAGE_SIZE);
             *pud |= PTE_P | PTE_RW | PTE_U;
@@ -150,13 +150,12 @@ static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
     pmd_t *pmd = &pud[pmd_idx];
 
 
-    if (!(*pmd & PTE_P)) {
+    if (!(*pmd & PTE_P) || init) {
 
         if (alloc) {
 
             *pmd = (uint64) kalloc(PAGE_SIZE);
             *pmd |=  PTE_P | PTE_RW | PTE_U;
-            panic("");
 
         } else {
             return 0;
@@ -166,7 +165,7 @@ static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
     pte_t *pte = &pmd[pte_idx];
 
 
-    if (!(*pte & PTE_P)) {
+    if (!(*pte & PTE_P) || init) {
         if (alloc) {
             *pte = (uint64) kalloc(PAGE_SIZE);
             *pte |=  PTE_P | PTE_RW | PTE_U;
@@ -182,14 +181,14 @@ static pte_t *walkpgdir(p4d_t *pgdir, const void *va, int alloc) {
 /*
  * Maps pages from VA/PA to size in page size increments.
  */
-int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 size) {
+int map_pages(p4d_t *pgdir, uint64 physaddr, uint64 *va, uint64 perms, uint64 size, int init) {
 
     uint64 address, last;
     pte_t *pte;
     address = PGROUNDDOWN((uint64) va);
     last = PGROUNDUP(((uint64) va) + size - 1);
     for (;;) {
-        if ((pte = walkpgdir(pgdir, (void *) address, 1)) == 0) {
+        if ((pte = walkpgdir(pgdir, (void *) address, 1,init)) == 0) {
             return -1;
         }
 
