4abc353da09a715be9b77ff74b1de3b2ebc8ca2a
dustyn <dustyn_gibb@hotmail.com>
2024-08-30 01:31:52 -0400

    Another dumbdumb this morning lol, fixed.

--- kernel/src/arch/x86_64/smp/arch_smp.c
+++ kernel/src/arch/x86_64/smp/arch_smp.c
@@ -38,10 +38,13 @@ void arch_smp_query(){
         cpu_list[i].cpu_number = smp_info[i]->processor_id;
         cpu_list[i].lapic_id= smp_info[i]->lapic_id;
         serial_printf("  CPU %x.8  LAPIC %x.8  initialized inside cpu_list\n",smp_info[i]->processor_id, smp_info[i]->lapic_id);
-        i++;
+
         //puts rest of CPUs online, works so will leave this commented out for now since I need to create or refactor functions for this
-        //smp_info[i]->goto_address = arch_initialise_cpu;
+        smp_info[i]->goto_address = arch_initialise_cpu;
+        i++;
     }
+
+    serial_printf("CPU0 DONE\n");
 
 
 }
