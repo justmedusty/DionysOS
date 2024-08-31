68758a37ee453e4a2255ac558af40c6e221f6a19
dustyn <dustyn_gibb@hotmail.com>
2024-08-31 04:36:57 -0400

    one more placeholder

---
+++ kernel/src/data_structures/red_black_tree.c
@@ -0,0 +1,5 @@
+//
+// Created by dustyn on 8/31/24.
+//
+
+#include "red_black_tree.h"
---
+++ kernel/src/include/data_structures/red_black_tree.h
@@ -0,0 +1,8 @@
+//
+// Created by dustyn on 8/31/24.
+//
+
+#ifndef RED_BLACK_TREE_H
+#define RED_BLACK_TREE_H
+
+#endif //RED_BLACK_TREE_H
