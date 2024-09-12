2d0442fc284c276aae12074c700c8b48e406e1fc
dustyn <dustyn_gibb@hotmail.com>
2024-09-12 05:42:02 -0400

    leaving it here for this morning

--- kernel/src/data_structures/queue.c
+++ kernel/src/data_structures/queue.c
@@ -16,7 +16,8 @@
  *  priority queue.
  */
 
-void queue_init(struct queue_node* queue_head, uint8 queue_mode,void *data,uint8 priority /* Only relevant for a priority queue */) {
+void queue_init(struct queue_node* queue_head, uint8 queue_mode, void* data,
+                uint8 priority /* Only relevant for a priority queue */) {
     queue_head->data = data;
     queue_head->next = NULL;
     queue_head->prev = NULL;
@@ -24,30 +25,24 @@ void queue_init(struct queue_node* queue_head, uint8 queue_mode,void *data,uint8
     queue_head->tail = queue_head;
     queue_head->queue_mode = queue_mode;
     queue_head->priority = priority;
-
 }
+
 /*
  *  Generic enqueue
  */
 
-void enqueue(struct queue_node* queue_head,void *data_to_enqueue,uint8 priority) {
-
+void enqueue(struct queue_node* queue_head, void* data_to_enqueue, uint8 priority) {
     uint8 queue_head_empty = 0;
-    if(queue_head == NULL) {
+    if (queue_head == NULL) {
         //So i can investigate when it happens, should have null data field not the entire head structure null
         panic("Null head");
     }
 
-    if(queue_head->data != NULL) {
-        struct queue_node *new_node = kalloc(sizeof(struct queue_node));
-        queue_init(new_node,queue_head->queue_mode,data_to_enqueue,priority);
-    }else {
-        queue_head->data = data_to_enqueue;
-    }
+    struct queue_node* new_node = kalloc(sizeof(struct queue_node));
+    queue_init(new_node, queue_head->queue_mode, data_to_enqueue, priority);
 
 
     switch (queue_head->queue_mode) {
-
     case QUEUE_MODE_FIFO:
         break;
 
@@ -72,14 +67,12 @@ void enqueue(struct queue_node* queue_head,void *data_to_enqueue,uint8 priority)
  *  Generic dequeue
  */
 void dequeue(struct queue_node* queue_head) {
-
-    if(queue_head == NULL) {
+    if (queue_head == NULL) {
         //So i can investigate when it happens, should have null data field not the entire head structure null
         panic("Null head");
     }
 
     switch (queue_head->queue_mode) {
-
     case QUEUE_MODE_FIFO:
         break;
 
@@ -101,21 +94,19 @@ void dequeue(struct queue_node* queue_head) {
 }
 
 
-void __enqueue_fifo(struct queue_node* queue_head, struct queue_node *new_node) {
-
+void __enqueue_fifo(struct queue_node* queue_head, struct queue_node* new_node) {
     /*
      * Base cases
      */
 
     //We will keep an empty queue head when the queue is empty , this makes more sense than throwing it out every time and reallocing.
-    if(queue_head->data == NULL) {
+    if (queue_head->data == NULL) {
         kfree(queue_head);
         queue_head = new_node;
         return;
     }
 
     if (queue_head->next == NULL) {
-
         queue_head->next = new_node;
         new_node->prev = queue_head;
         queue_head->tail = new_node;
@@ -128,7 +119,6 @@ void __enqueue_fifo(struct queue_node* queue_head, struct queue_node *new_node) 
     new_node->prev = queue_head->tail;
     queue_head->tail = new_node;
     new_node->head = queue_head->tail;
-
 }
 
 /* Push */
@@ -136,13 +126,12 @@ void __enqueue_fifo(struct queue_node* queue_head, struct queue_node *new_node) 
 /*
  *  Should I even bother with tail maintenance here? Unsure it could help if I want to mark pseudo frames or something. I'll sit on it for a bit.
  */
-void __enqueue_lifo(struct queue_node* queue_head, struct queue_node *new_node) {
-
+void __enqueue_lifo(struct queue_node* queue_head, struct queue_node* new_node) {
     /*
     * Base cases
     */
 
-    if(queue_head->data == NULL) {
+    if (queue_head->data == NULL) {
         kfree(queue_head);
         queue_head = new_node;
         return;
@@ -151,13 +140,28 @@ void __enqueue_lifo(struct queue_node* queue_head, struct queue_node *new_node) 
     new_node->next = queue_head;
     queue_head->prev = new_node;
     new_node->tail = queue_head->tail;
-    queue_head=new_node;
-
+    queue_head = new_node;
 }
 
-void __enqueue_priority(struct queue_node* queue_head, struct queue_node *new_node) {
+void __enqueue_priority(struct queue_node* queue_head, struct queue_node* new_node) {
 
-    if(queue_head->data == NULL) {
+    if (queue_head->data == NULL) {
+        kfree(queue_head);
+        queue_head = new_node;
+        return;
+    }
+    struct queue_node* pointer = queue_head;
+
+    for(;;) {
+
+        if (pointer->next == NULL) {
+            break;
+        }
+
+        if (pointer->priority < new_node->priority) {
+
+        }
+
 
     }
 }
