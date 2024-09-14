11379cc293696dff2860f2ce0ebf1b07b054c5ca
dustyn <dustyn_gibb@hotmail.com>
2024-09-14 04:03:44 -0400

    Touched up on singly linked list implementation

--- kernel/src/data_structures/singly_linked_list.c
+++ kernel/src/data_structures/singly_linked_list.c
@@ -13,80 +13,74 @@
  *  I will leave out things like sorts since I am trying to make this as generic as possible and I can't sort without knowing the type the data pointer leads to.
  */
 
-void singly_linked_list_init(struct singly_linked_list_node* head) {
-    head->next = NULL;
-    head->data = NULL;
+void singly_linked_list_init(struct singly_linked_list* list) {
+    list->head = NULL;
+    list->tail = NULL;
+    list->node_count = 0;
 }
 
-void singly_linked_list_insert_tail(struct singly_linked_list_node* head, void* data) {
-    struct singly_linked_list_node* node = head;
+void singly_linked_list_insert_tail(struct singly_linked_list* list, void* data) {
 
-    while (node->next != NULL) {
-        node = node->next;
-    }
-
-    node->next = kalloc(sizeof(struct singly_linked_list_node));
-    node->next->data = data;
+    struct singly_linked_list_node *new_node = kalloc(sizeof(struct singly_linked_list_node));
+    new_node->data = data;
+    list->tail->next = new_node;
+    list->tail = new_node;
+    list->node_count++;
 }
 
-struct singly_linked_list_node* singly_linked_list_insert_head(struct singly_linked_list_node* old_head, void* data) {
+void singly_linked_list_insert_head(struct singly_linked_list* list, void* data) {
     struct singly_linked_list_node* new_head = kalloc(sizeof(struct singly_linked_list_node));
     new_head->data = data;
-    new_head->next = old_head;
-    return new_head;
+    new_head->next = list->head;
+    list->head = new_head;
+    list->node_count++;
 }
 
 
-void singly_linked_list_remove_tail(struct singly_linked_list_node* head) {
-    struct singly_linked_list_node* node = head;
-    struct singly_linked_list_node* new_tail = NULL;
+void singly_linked_list_remove_tail(struct singly_linked_list* list) {
 
-    if(node->next == NULL) {
-        kfree(node);
+    if(list->node_count == 0) {
         return;
     }
 
-    while (node->next != NULL) {
-        node = node->next;
-        if (node->next->next == NULL) {
-            new_tail = node->next;
-            node = node->next;
-            break;
+    if(list->node_count == 1) {
+        kfree(list->head);
+        list->head = NULL;
+        list->tail = NULL;
+        list->node_count--;
+        return;
+    }
+
+    struct singly_linked_list_node* node = list->head;
+    kfree(list->tail);
+    list->tail = NULL;
+
+    while (node != NULL) {
+        if(node->next == NULL) {
+            list->tail = node;
+
         }
+        node = node->next;
     }
-    new_tail->next = NULL;
-    kfree(node);
+
+    list->node_count--;
 }
 
-struct singly_linked_list_node* singly_linked_list_remove_head(struct singly_linked_list_node* head) {
-    if (head->next == NULL) {
-        kfree(head);
-        return NULL;
+void singly_linked_list_remove_head(struct singly_linked_list* list) {
+    if (list->node_count == 0) {
+        return;
     }
 
-    struct singly_linked_list_node* new_head = head->next;
-    kfree(head);
-    return new_head;
-}
-
-/*
- *  I will set up tests to test my generic data structure interaction functions and just panic the result. Will only be run once manually and then won't be called until a modification is made during the development process.
- */
-
-void linked_list_test() {
-    struct singly_linked_list_node* head = kalloc(sizeof(struct singly_linked_list_node));
-    struct singly_linked_list_node** nodes;
-
-    for (int i = 0; i < 10; i++) {
-        singly_linked_list_insert_tail(head,nodes[i]);
+    if(list->node_count == 1) {
+        kfree(list->head);
+        list->head = NULL;
+        list->tail = NULL;
+        list->node_count--;
+        return;
     }
 
-    for (int i = 0; i < 10; i++) {
-        //if(head->next)
-    }
-
-
-
-
-
+    struct singly_linked_list_node* new_head = list->head->next;
+    kfree(list->head);
+    list->head = new_head;
+    list->node_count--;
 }
--- kernel/src/include/data_structures/singly_linked_list.h
+++ kernel/src/include/data_structures/singly_linked_list.h
@@ -11,12 +11,14 @@ struct singly_linked_list_node{
 
 struct singly_linked_list {
   struct singly_linked_list_node* head;
+  //I will keep tail now anyway but you cannot really use it to skip the line since you cant update its predecessors pointer without a walk. It may be useful in the case we need to insert something at the end however.
   struct singly_linked_list_node* tail;
   uint64 node_count;
 };
 
-void singly_linked_list_init(struct singly_linked_list_node *head);
-void singly_linked_list_insert_tail(struct singly_linked_list_node *head, void *data);
-struct singly_linked_list_node*  singly_linked_list_insert_head(struct singly_linked_list_node *old_head, void *data);
-void singly_linked_list_remove_tail(struct singly_linked_list_node* head);
-struct singly_linked_list_node* singled_linked_list_remove_head(struct singly_linked_list_node* head);
+
+void singly_linked_list_init(struct singly_linked_list *list);
+void singly_linked_list_insert_tail(struct singly_linked_list *list, void *data);
+void singly_linked_list_insert_head(struct singly_linked_list *list, void *data);
+void singly_linked_list_remove_tail(struct singly_linked_list *list);
+void singled_linked_list_remove_head(struct singly_linked_list *list);
