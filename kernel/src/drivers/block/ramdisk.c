d9d8757d95bd9722b7a6091db0ade559c9a023a2
dustyn <dustyn_gibb@hotmail.com>
2024-10-26 04:26:55 -0400

    Ramdisk changes, multiple disks and a struct for it.

--- kernel/src/drivers/block/ramdisk.c
+++ kernel/src/drivers/block/ramdisk.c
@@ -4,40 +4,50 @@
 
 #include "include/filesystem/ramdisk.h"
 #include <include/types.h>
+#include <include/drivers/uart.h>
 #include <include/filesystem/tempfs.h>
 #include <include/mem/kalloc.h>
 #include "include/mem/mem.h"
 #include "include/mem/pmm.h"
 
-#define DEFAULT_RAMDISK_SIZE (0xFF * PAGE_SIZE)
-static void *ramdisk_start_address;
-static void *ramdisk_end_address;
-static uint64 ramdisk_size;
+struct ramdisk ramdisk[3]; /* 3 Only need one but why not 3! */
+uint64 ramdisk_count = sizeof(ramdisk) / sizeof(ramdisk[0]);
 
+void ramdisk_init(const uint64 size,const uint64 ramdisk_id) {
 
-void ramdisk_init(uint64 size) {
+        if(ramdisk_id > ramdisk_count) {
+                serial_printf("ramdisk id is out of range\n");
+                return;
+        }
         uint64 pages = size / PAGE_SIZE;
         if (pages == 0) {
                 pages = DEFAULT_RAMDISK_SIZE;
         }
-        ramdisk_start_address = kalloc(pages);
-        ramdisk_size = pages;
-        ramdisk_end_address = ramdisk_start_address + (pages * PAGE_SIZE);
+        ramdisk[ramdisk_id].ramdisk_start = kalloc(pages);
+         ramdisk[ramdisk_id].ramdisk_size_pages = pages;
+         ramdisk[ramdisk_id].ramdisk_end =  ramdisk[ramdisk_id].ramdisk_start + (pages * PAGE_SIZE);
 }
 
-uint64 ramdisk_mkfs(int8 *initramfs_img,uint64 size_bytes) {
-        if(size_bytes < ramdisk_size) {
+uint64 ramdisk_mkfs(const int8 *initramfs_img,const uint64 size_bytes, const uint64 ramdisk_id) {
+        if(ramdisk_id > ramdisk_count) {
+                return RAMDISK_ID_OUT_OF_RANGE;
+        }
+        if((size_bytes / PAGE_SIZE) < ramdisk[ramdisk_id].ramdisk_size_pages) {
                 return RAMDISK_SIZE_TOO_SMALL;
         }
-        memcpy(ramdisk_start_address, initramfs_img, size_bytes);
+        memcpy(ramdisk[ramdisk_id].ramdisk_start, initramfs_img, size_bytes);
         return SUCCESS;
 }
 
-void ramdisk_destroy() {
-        kfree(ramdisk_start_address);
-        ramdisk_start_address = NULL;
-        ramdisk_size = 0;
-        ramdisk_end_address = NULL;
+void ramdisk_destroy(const uint64 ramdisk_id) {
+        if(ramdisk_id > ramdisk_count) {
+                serial_printf("ramdisk id is out of range\n");
+                return;
+        }
+        kfree(ramdisk[ramdisk_id].ramdisk_start);
+        ramdisk[ramdisk_id].ramdisk_start = NULL;
+        ramdisk[ramdisk_id].ramdisk_size_pages = 0;
+        ramdisk[ramdisk_id].ramdisk_end = NULL;
 }
 
 /*
--- kernel/src/include/filesystem/ramdisk.h
+++ kernel/src/include/filesystem/ramdisk.h
@@ -4,13 +4,30 @@
 
 #pragma once
 #include "include/types.h"
+#include "include/data_structures/spinlock.h"
 
 #define SUCCESS 0
 #define RAMDISK_SIZE_TOO_SMALL 0x4
+#define RAMDISK_ID_OUT_OF_RANGE 0x8
+
+#define DEFAULT_RAMDISK_SIZE (0xFF * PAGE_SIZE)
+#define RAMDISK_TEMPFS_ID 0x1
+#define RAMDISK_EXT2_ID 0x2
 
 
-void ramdisk_init(uint64 size);
-uint64 ramdisk_mkfs(int8 *initramfs_img,uint64 size_bytes);
-void ramdisk_destroy();
+struct ramdisk {
+    void *ramdisk_start;
+    void *ramdisk_end;
+    uint64 ramdisk_size_pages;
+    uint64 block_size;
+    struct spinlock ramdisk_lock;
+    uint16 file_system_id;
+    char ramdisk_name[32];
+};
+
+
+void ramdisk_init(const uint64 size,const uint64 ramdisk_id);
+uint64 ramdisk_mkfs(const int8 *initramfs_img,const uint64 size_bytes, const uint64 ramdisk_id);
+void ramdisk_destroy(const uint64 ramdisk_id);
 uint64 ramdisk_read(uint8 *buffer, uint64 block, uint64 offset, uint64 read_size,uint64 buffer_size);
 uint64 ramdisk_write(uint8 *buffer, uint64 block, uint64 offset, uint64 write_size,uint64 buffer_size);
