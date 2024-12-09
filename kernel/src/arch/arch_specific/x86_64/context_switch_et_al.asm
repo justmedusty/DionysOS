690272b064860616514f1fc59f410d17e7516362
dustyn <dustyn_gibb@hotmail.com>
2024-12-09 06:28:58 -0500

    small changes to context_switch_et_al.asm

--- kernel/src/arch/arch_specific/x86_64/context_switch_et_al.asm
+++ kernel/src/arch/arch_specific/x86_64/context_switch_et_al.asm
@@ -10,8 +10,7 @@ get_regs: ;pointer should be in RDI
     mov [rdi + 40], rsi       ; gpr_state.rsi = rsi
     mov [rdi + 48], rbp       ; gpr_state.rbp = rbp
     mov [rdi + 56], rsp       ; gpr_state.rsp = rsp
-    mov rax, [rsp + 0]
-    mov [rdi + 64], rax  ; gpr_state.rip = the start of the stack frame (interrupt only)
+
     mov [rdi + 72], r8        ; gpr_state.r8 = r8
     mov [rdi + 80], r9        ; gpr_state.r9 = r9
     mov [rdi + 88], r10       ; gpr_state.r10 = r10
@@ -20,6 +19,9 @@ get_regs: ;pointer should be in RDI
     mov [rdi + 112], r13      ; gpr_state.r13 = r13
     mov [rdi + 120], r14      ; gpr_state.r14 = r14
     mov [rdi + 128], r15      ; gpr_state.r15 = r15
+    pop rax ; pop rip into rax
+    mov [rdi + 64], rax ; rip
+    push rax;
     ret
 
 
@@ -35,7 +37,6 @@ restore_execution ;pointer should be in RDI
     mov rsi, [rdi + 40]       ; rsi = gpr_state.rsi
     mov rbp, [rdi + 48]       ; rbp = gpr_state.rbp
     mov rsp, [rdi + 56]       ; rsp = gpr_state.rsp
-    mov rax, [rdi + 64]       ; rax = gpr_state.rip (from saved stack frame)
     mov r8, [rdi + 72]        ; r8 = gpr_state.r8
     mov r9, [rdi + 80]        ; r9 = gpr_state.r9
     mov r10, [rdi + 88]       ; r10 = gpr_state.r10
@@ -44,7 +45,7 @@ restore_execution ;pointer should be in RDI
     mov r13, [rdi + 112]      ; r13 = gpr_state.r13
     mov r14, [rdi + 120]      ; r14 = gpr_state.r14
     mov r15, [rdi + 128]      ; r15 = gpr_state.r15
-    jmp rax
+    jmp [rdi + 64]
     
     
 global context_switch ; This will likely not work as is will need to modify it. context_switch(struct gpr_state *old, struct gpr_state *new)
