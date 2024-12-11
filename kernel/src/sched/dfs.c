7a0dbc174c495e86f551d5ba5f5248de8d6be240
dustyn <dustyn_gibb@hotmail.com>
2024-12-11 15:06:11 -0500

    added a process search function for when a cpus local queue is empty

--- kernel/src/sched/dfs.c
+++ kernel/src/sched/dfs.c
@@ -7,13 +7,11 @@
  */
 #define _DFS_ // this is only here because clion is complaining and I lose intellisense because reading a makefile is too difficult and complicated. This is meant to just be defined in the makefile
 
+#ifdef _DFS_
 #include <include/arch/arch_asm_functions.h>
 #include <include/arch/arch_timer.h>
 #include <include/data_structures/doubly_linked_list.h>
 #include <include/data_structures/singly_linked_list.h>
-#ifdef _DFS_
-
-
 #include "include/scheduling/sched.h"
 #include <include/definitions.h>
 #include <include/data_structures/spinlock.h>
@@ -26,13 +24,16 @@
 #include <include/mem/mem.h>
 
 
-struct queue sched_global_queue;
+struct queue sched_global_queue; // this is where processes will be stuck for CPUs to poach when they aren't busy
 struct queue global_sleep_queue;
 
 struct spinlock sched_global_lock;
+struct spinlock sched_sleep_lock;
 struct spinlock purge_lock;
 struct singly_linked_list dead_processes;
+
 static void purge_dead_processes();
+static void look_for_process();
 extern void context_switch(struct gpr_state *old,struct gpr_state *new);
 
 static void free_process(struct process *process) {
@@ -56,7 +57,9 @@ void sched_init() {
   singly_linked_list_init(&dead_processes,0);
   initlock(&sched_global_lock, sched_LOCK);
   initlock(&purge_lock, sched_LOCK);
+  initlock(&sched_sleep_lock, sched_LOCK);
   queue_init(&sched_global_queue,QUEUE_MODE_FIFO,"sched_global");
+  queue_init(&global_sleep_queue,QUEUE_MODE_FIFO,"global_sleep");
   for(uint32_t i = 0; i < cpu_count; i++) {
     queue_init(&local_run_queues[i],QUEUE_MODE_FIFO,"dfs");
     cpu_list[i].local_run_queue = &local_run_queues[i];
@@ -86,7 +89,8 @@ void sched_run() {
   if (cpu->local_run_queue->head == NULL) {
     timer_sleep(1000);
     serial_printf("DFS: Local Run Queue is Empty \n");
-    purge_dead_processes();
+    purge_dead_processes(); /* This doesn't allow for an explicit wait maybe I will change that later*/
+    look_for_process();
     return;
   }
 
@@ -105,7 +109,15 @@ void sched_preempt() {
   context_switch(my_cpu()->running_process->current_gpr_state,cpu->scheduler_state);
 }
 
+
+void sched_sleep(void *sleep_channel) {
+  struct cpu* cpu = my_cpu();
+  struct process *process = cpu->running_process;
+
+}
+
 void sched_claim_process() {
+
 }
 
 void sched_exit() {
@@ -130,6 +142,21 @@ static void purge_dead_processes() {
     node = dead_processes.head;
   }
   release_spinlock(&purge_lock);
+}
+
+static void look_for_process() {
+  struct cpu *cpu = my_cpu();
+  acquire_spinlock(&sched_global_lock);
+
+  if (sched_global_queue.queue_mode == 0) {
+    release_spinlock(&sched_global_lock);
+    return;
+  }
+  struct process *process = sched_global_queue.head->data;
+  dequeue(&sched_global_queue);
+  enqueue(cpu->local_run_queue,process,process->priority);
+  release_spinlock(&sched_global_lock);
+  return;
 }
 
 #endif
