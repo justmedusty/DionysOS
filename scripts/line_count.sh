61cfe247fc2f6248d5d341ac63876698debdbd9d
Dustyn <dustyn_gibb@hotmail.com>
2025-01-01 04:22:53 -0500

    added asm files as well

--- scripts/line_count.sh
+++ scripts/line_count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
 echo Lines of code so far: 
-find -type f -name *.c -o -name *.h ! -name "*limine*"  | xargs wc -l | tail -1 | sed 's/[^0-9]*//g'
+find -type f -name *.c -o -name *.h -o -name *.asm ! -name "*limine*"  | xargs wc -l | tail -1 | sed 's/[^0-9]*//g'
