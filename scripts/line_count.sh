3099d2da8a07a95a09fe371d110a9edf90ffc811
Dustyn <dustyn_gibb@hotmail.com>
2025-01-01 04:24:45 -0500

    made the echo msg more descriptive

--- scripts/line_count.sh
+++ scripts/line_count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ..
-echo Lines of code so far: 
+echo Lines of code so far including .c files, .h files, and .asm files: 
 find -type f -name *.c -o -name *.h -o -name *.asm ! -name "*limine*"  | xargs wc -l | tail -1 | sed 's/[^0-9]*//g'
