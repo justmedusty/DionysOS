9959d05f57915da7878a69ff69c9a16625634110
dustyn <dustyn_gibb@hotmail.com>
2025-01-06 04:06:32 -0500

    mutex files

---
+++ kernel/src/data_structures/concurrency/mutex.c
@@ -0,0 +1,8 @@
+//
+// Created by dustyn on 1/6/25.
+//
+#include <stdint.h>
+#include "include/data_structures/mutex.h"
+#include "include/architecture/arch_atomic_operations.h"
+
+
---
+++ kernel/src/include/data_structures/mutex.h
@@ -0,0 +1,8 @@
+//
+// Created by dustyn on 1/6/25.
+//
+
+#ifndef KERNEL_MUTEX_H
+#define KERNEL_MUTEX_H
+
+#endif //KERNEL_MUTEX_H
