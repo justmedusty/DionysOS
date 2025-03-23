3ce8893f7bd53ad9476db856006a731e5dc2556c
dustyn <dustyn_gibb@hotmail.com>
2025-03-23 11:46:40 -0400

    gonna start to set up syscalls

--- kernel/src/include/system_call/system_calls.h
+++ kernel/src/include/system_call/system_calls.h
@@ -5,4 +5,17 @@
 #ifndef SYSTEM_CALLS_H
 #define SYSTEM_CALLS_H
 
+enum system_calls {
+    SYS_WRITE,
+    SYS_READ,
+    SYS_EXIT,
+    SYS_SPAWN,
+    SYS_EXEC,
+    SYS_OPEN,
+    SYS_CLOSE,
+    SYS_CREATE,
+    SYS_ALLOC,
+    SYS_DEALLOC,
+
+};
 #endif //SYSTEM_CALLS_H
