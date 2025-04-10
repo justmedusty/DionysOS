7e288512a2b7853aebcbe9bf8bb7e7c444b993d4
dustyn <dustyn_gibb@hotmail.com>
2025-04-10 04:40:42 -0400

    inline directive to syscall stub

--- kernel/src/userspace/systemcall_wrappers.h
+++ kernel/src/userspace/systemcall_wrappers.h
@@ -6,5 +6,6 @@
 #define KERNEL_SYSTEMCALL_WRAPPERS_H
 #pragma once
 #include "stdint.h"
+__attribute__((always_inline))
 int syscall_stub(uint64_t syscall_number, uint64_t arg1,uint64_t arg2,uint64_t arg3,uint64_t arg4,uint64_t arg5,uint64_t arg6);
 #endif //KERNEL_SYSTEMCALL_WRAPPERS_H
