e1dd824df3ad9fba860d92953f4028e8a838a6c6
dustyn <dustyn_gibb@hotmail.com>
2025-06-03 04:24:12 -0400

    Renaming syscall stubs since it appears these might have been mistakenly linked with kernel definitions

--- kernel/src/userspace/systemcall_wrappers.c
+++ kernel/src/userspace/systemcall_wrappers.c
@@ -5,42 +5,42 @@
 #include "include/system_call/system_calls.h"
 
 
-static inline int64_t open(char *path) {
+static inline int64_t sys_open(char *path) {
     return syscall_stub((uint64_t)SYS_OPEN, (uint64_t)path, 0, 0, 0, 0, 0);
 }
 
-static inline int64_t write(uint64_t handle, char *buffer, uint64_t bytes) {
+static inline int64_t sys_write(uint64_t handle, char *buffer, uint64_t bytes) {
 return syscall_stub((uint64_t)SYS_WRITE, handle, (uint64_t)buffer, bytes, 0, 0, 0);
 }
 
-static inline int64_t read(uint64_t handle, char *buffer, uint64_t bytes) {
+static inline int64_t sys_read(uint64_t handle, char *buffer, uint64_t bytes) {
 return syscall_stub((uint64_t)SYS_READ, handle, (uint64_t)buffer, bytes, 0, 0, 0);
 }
 
-static inline int64_t close(uint64_t handle) {
+static inline int64_t sys_close(uint64_t handle) {
 return syscall_stub((uint64_t)SYS_CLOSE, handle, 0, 0, 0, 0, 0);
 }
 
-static inline int64_t mount(char *mount_point, char *mounted_filesystem) {
+static inline int64_t sys_mount(char *mount_point, char *mounted_filesystem) {
     return syscall_stub((uint64_t)SYS_MOUNT, (uint64_t)mount_point, (uint64_t)mounted_filesystem, 0, 0, 0, 0);
 }
 
-static inline int64_t unmount(char *path) {
+static inline int64_t sys_unmount(char *path) {
     return syscall_stub((uint64_t)SYS_UNMOUNT, (uint64_t)path, 0, 0, 0, 0, 0);
 }
 
-static inline int64_t rename(char *path, char *new_name) {
+static inline int64_t sys_rename(char *path, char *new_name) {
     return syscall_stub((uint64_t)SYS_RENAME, (uint64_t)path, (uint64_t)new_name, 0, 0, 0, 0);
 }
 
-static inline int64_t create(char *path, char *name, uint64_t type) {
+static inline int64_t sys_create(char *path, char *name, uint64_t type) {
     return syscall_stub((uint64_t)SYS_CREATE, (uint64_t)path, (uint64_t)name, type, 0, 0, 0);
 }
 
-static inline int64_t seek(uint64_t handle, uint64_t whence) {
+static inline int64_t sys_seek(uint64_t handle, uint64_t whence) {
 return syscall_stub((uint64_t)SYS_SEEK, handle, whence, 0, 0, 0, 0);
 }
 
-static inline int64_t exit() {
+static inline int64_t sys_exit() {
 return syscall_stub((uint64_t)SYS_EXIT, 0, 0, 0, 0, 0, 0);
 }
