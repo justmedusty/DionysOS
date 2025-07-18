115b4ab4680ef683a1a822be4ac1b1926f901919
Dustyn Gibb <dustyn_gibb@hotmail.com>
2025-07-18 03:14:45 -0400

    syscall wrapper changed to inline asm stub

--- kernel/src/userspace/systemcall_wrappers.h
+++ kernel/src/userspace/systemcall_wrappers.h
@@ -29,8 +29,30 @@ enum system_calls {
 };
 
 
-int syscall_stub(uint64_t syscall_number, uint64_t arg1,uint64_t arg2,uint64_t arg3,uint64_t arg4,uint64_t arg5,uint64_t arg6);
+#ifdef __x86_64__
+static __inline__ __attribute__((always_inline)) int64_t syscall_stub(
+    uint64_t syscall_no,
+    uint64_t arg1, uint64_t arg2, uint64_t arg3,
+    uint64_t arg4, uint64_t arg5, uint64_t arg6)
+{
+    uint64_t ret;
+    register uint64_t r10 __asm__("r10") = arg4;
+    register uint64_t r8  __asm__("r8")  = arg5;
+    register uint64_t r9  __asm__("r9")  = arg6;
 
+    __asm__ __volatile__ (
+        "syscall"
+        : "=a"(ret)
+        : "a"(syscall_no),
+          "D"(arg1),
+          "S"(arg2),
+          "d"(arg3),
+          "r"(r10), "r"(r8), "r"(r9)
+        : "rcx", "r11", "memory"
+    );
+    return ret;
+}
+#endif
 
 static inline int64_t sys_open(char *path) {
     return syscall_stub((uint64_t)SYS_OPEN, (uint64_t)path, 0, 0, 0, 0, 0);
