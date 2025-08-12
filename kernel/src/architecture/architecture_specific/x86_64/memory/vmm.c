7f363c518f938c7660ebd56618111f0f6b584044
Dustyn Gibb <dustyn_gibb@hotmail.com>
2025-08-12 03:19:08 -0400

    trying to fix free page tables func

--- kernel/src/architecture/architecture_specific/x86_64/memory/vmm.c
+++ kernel/src/architecture/architecture_specific/x86_64/memory/vmm.c
@@ -148,13 +148,6 @@ pte_t *walk_page_directory(p4d_t *pgdir, const void *va, const int flags) {
     return entry;
 }
 
-static void *get_next_level(void *table, uint64_t index) {
-    uint64_t *entries = (uint64_t *) Phys2Virt(table);
-    if (!(entries[index] & PTE_P)) {
-        return NULL;
-    }
-    return Phys2Virt(PTE_ADDR(entries[index]));
-}
 
 /*
  * Maps pages from VA/PA to size in page size increments.
@@ -215,19 +208,30 @@ void dealloc_va_range(p4d_t *pgdir, const uint64_t address, const uint64_t size)
     panic("");
 }
 #define ENTRIES_PER_TABLE 512
+
+static void *get_next_level(void *table, uint64_t index) {
+    uint64_t *entries = (uint64_t *) Phys2Virt(table);
+    kprintf("ENTRIES %x.64\n",entries);
+    if (!(entries[index] & PTE_P)) {
+        return NULL;
+    }
+    return (void *)(PTE_ADDR(entries[index]));
+}
+
 void free_page_tables(p4d_t *pgdir) {
+    kprintf("HERE\n");
     for (int p4d_idx = 0; p4d_idx < ENTRIES_PER_TABLE; p4d_idx++) {
         pud_t *pud = get_next_level(pgdir, p4d_idx);
         if (!pud) continue;
-
+        kprintf("HERE2\n");
         for (int pud_idx = 0; pud_idx < ENTRIES_PER_TABLE; pud_idx++) {
             pmd_t *pmd = get_next_level(pud, pud_idx);
-            if (!pmd) continue;
-
+            if (!pmd) continue;\
+            kprintf("HERE3\n");
             for (int pmd_idx = 0; pmd_idx < ENTRIES_PER_TABLE; pmd_idx++) {
                 pte_t *pte = get_next_level(pmd, pmd_idx);
                 if (!pte) continue;
-
+                kprintf("HERE4\n");
                 for (int pte_idx = 0; pte_idx < ENTRIES_PER_TABLE; pte_idx++) {
                     if (pte[pte_idx] & PTE_P) {
                         void *page = (void *) PTE_ADDR(pte[pte_idx]);
@@ -242,6 +246,7 @@ void free_page_tables(p4d_t *pgdir) {
         kfree(pud);
     }
     kfree(pgdir);
+
 }
 
 void setup_pat() {
