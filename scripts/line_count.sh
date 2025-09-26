4219a642a012aa7794bb6459eae6d641612026e1
dustyn <dustyn_gibb@hotmail.com>
2025-09-26 02:08:42 -0400

    Added check in linecount.sh to ensure its being run properly

--- scripts/line_count.sh
+++ scripts/line_count.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [[ ! -e ./line_count.sh ]]; then
+  echo "This script must be run from the actual directory the script resides in!"
+  exit
+fi
 cd ..
 echo Lines of code so far including .c files, .h files, and .asm files: 
 find -type f \( -name "*.c" -o -name "*.h" -o -name "*.asm" \) -not -name "*limine*" | xargs wc -l | tail -1 | sed 's/[^0-9]*//g'
